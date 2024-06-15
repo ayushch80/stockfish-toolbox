19959d64d27574b09727f78c14b65f0311af2679
ayushch80 <ayushch80@gmail.com>
2024-06-15 15:53:55 +0530

    Fixed outDir for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './src',
+  outDir: './dist',
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
